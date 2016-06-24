2d31282a29f43bfc68816227ad1909a2346ec4de
JasonArkens17 <jason.arkens17@gmail.com>
2016-06-24 16:02:06 -0700

    Render Video list to page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ var App = () => (
       <VideoPlayer/>
     </div>
     <div className="col-md-5">
-      <VideoList/>
+      <VideoList videos={exampleVideoData}/>
     </div>
   </div>
 );
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,9 @@
-var VideoList = () => (
+
+var VideoList = (props) => (
   <div className="video-list media">
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
-    <VideoListEntry />
+  {props.videos.map(video => 
+    <VideoListEntry video={video} />
+  )}
   </div>
 );
 
