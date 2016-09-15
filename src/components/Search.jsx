9008fd22b6db7b58a01ceb34da4c9405b2358489
Jason Davenport <jason.arkens17@gmail.com>
2016-09-15 15:02:56 -0700

    search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,50 @@
-var Search = (props) => (
+// This is the conrolled component solution
+// https://facebook.github.io/react/docs/forms.html#controlled-components
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleInputChange(e) {
+    this.props.handleSearchInputChange(e.target.value);
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input
+          className="form-control"
+          type="text"
+          value={this.state.value}
+          onChange={this.handleInputChange.bind(this)}
+        />
+        <button className="btn hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div>
+    );
+  }
+}
+// This is the uncontrolled component solution
+// It is not used, just here for reference
+var UncontrolledSearch = ({handleSearchInputChange}) => (
   <div className="search-bar form-inline">
-    <input onChange={() => props.btnSearch()} className="form-control" type="text" />
-    <button onClick={() => props.btnSearch()} className="btn hidden-sm-down">
+    <input
+      className="form-control"
+      type="text"
+      onChange={(e) => handleSearchInputChange(e.target.value)}
+    />
+    <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
-  </div> 
+  </div>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
